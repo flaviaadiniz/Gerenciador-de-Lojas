ec05c1809a1fdb16def6a38b886099d19235df8f
flaviaadiniz <flaviaadiniz@gmail.com>
2023-04-17 11:15:47 -0300

    Adição dos atributos e métodos da classe Loja

--- src/main/java/Loja.java
+++ src/main/java/Loja.java
@@ -1,2 +1,69 @@
-package PACKAGE_NAME;public class Loja {
+public class Loja {
+
+    private String nome;
+    private int quantidadeFuncionarios;
+    private double salarioBaseFuncionario;
+
+    public Loja(String nome, int quantidadeFuncionarios, double salarioBaseFuncionario) {
+        this.nome = nome;
+        this.quantidadeFuncionarios = quantidadeFuncionarios;
+        this.salarioBaseFuncionario = salarioBaseFuncionario;
+    }
+
+    public Loja(String nome, int quantidadeFuncionarios) {
+        this.nome = nome;
+        this.quantidadeFuncionarios = quantidadeFuncionarios;
+        this.salarioBaseFuncionario = -1;
+    }
+
+    public double gastosComSalario() {
+        return this.quantidadeFuncionarios * this.salarioBaseFuncionario;
+    }
+
+    public char tamanhoDaLoja() {
+        char tamanho = ' ';
+
+        if (this.quantidadeFuncionarios < 10) {
+            tamanho = 'P';
+        } else if (this.quantidadeFuncionarios >= 10 && this.quantidadeFuncionarios <= 30) {
+            tamanho = 'M';
+        } else if (this.quantidadeFuncionarios > 30) {
+            tamanho = 'G';
+        }
+
+        return tamanho;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getQuantidadeFuncionarios() {
+        return quantidadeFuncionarios;
+    }
+
+    public void setQuantidadeFuncionarios(int quantidadeFuncionarios) {
+        this.quantidadeFuncionarios = quantidadeFuncionarios;
+    }
+
+    public double getSalarioBaseFuncionario() {
+        return salarioBaseFuncionario;
+    }
+
+    public void setSalarioBaseFuncionario(double salarioBaseFuncionario) {
+        this.salarioBaseFuncionario = salarioBaseFuncionario;
+    }
+
+    @Override
+    public String toString() {
+        return "Loja [" +
+                "Nome: " + nome +
+                "\nQuantidade de Funcionários: " + quantidadeFuncionarios +
+                "\nSalário Base do Funcionario: " + salarioBaseFuncionario +
+                ']';
+    }
 }
