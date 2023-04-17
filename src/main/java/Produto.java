124a021d9b47fd1015bf2caffe969e8f1410c8fb
flaviaadiniz <flaviaadiniz@gmail.com>
2023-04-17 11:21:22 -0300

    Adição dos atributos e métodos da classe Produto

--- src/main/java/Produto.java
+++ src/main/java/Produto.java
@@ -1,2 +1,34 @@
-package PACKAGE_NAME;public class Produto {
+public class Produto {
+
+    private String nome;
+    private double preco;
+
+    public Produto(String nome, double preco) {
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    @Override
+    public String toString() {
+        return "Produto [" +
+                "Nome: " + nome +
+                " | Preço: " + preco +
+                ']';
+    }
 }
