5cdec09298712ba4d66c7452b393aae463866524
flaviaadiniz <flaviaadiniz@gmail.com>
2023-05-12 18:09:35 -0300

    Adição da classe Vestuario

---
+++ src/main/java/Vestuario.java
@@ -0,0 +1,24 @@
+public class Vestuario extends Loja {
+
+    private boolean produtosImportados;
+
+    public Vestuario(String nome, int quantidadeFuncionarios, double salarioBaseFuncionario, Endereco endereco,
+                     Data dataFundacao, boolean produtosImportados) {
+        super(nome, quantidadeFuncionarios, salarioBaseFuncionario, endereco, dataFundacao);
+        this.produtosImportados = produtosImportados;
+    }
+
+    public boolean isProdutosImportados() {
+        return produtosImportados;
+    }
+
+    public void setProdutosImportados(boolean produtosImportados) {
+        this.produtosImportados = produtosImportados;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() +
+                "\nVende produtos importados: " + produtosImportados;
+    }
+}
