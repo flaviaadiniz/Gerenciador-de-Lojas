6067322057314accdf8ca221e189eda3d67713e4
flaviaadiniz <flaviaadiniz@gmail.com>
2023-05-23 22:59:13 -0300

    Atualização do construtor das classes que herdam de Loja

--- src/main/java/Vestuario.java
+++ src/main/java/Vestuario.java
@@ -3,12 +3,12 @@ public class Vestuario extends Loja {
     private boolean produtosImportados;
 
     public Vestuario(String nome, int quantidadeFuncionarios, double salarioBaseFuncionario, Endereco endereco,
-                     Data dataFundacao, boolean produtosImportados) {
-        super(nome, quantidadeFuncionarios, salarioBaseFuncionario, endereco, dataFundacao);
+                     Data dataFundacao, boolean produtosImportados, int quantidadeProdutos) {
+        super(nome, quantidadeFuncionarios, salarioBaseFuncionario, endereco, dataFundacao, quantidadeProdutos);
         this.produtosImportados = produtosImportados;
     }
 
-    public boolean isProdutosImportados() {
+    public boolean getProdutosImportados() {
         return produtosImportados;
     }
 
