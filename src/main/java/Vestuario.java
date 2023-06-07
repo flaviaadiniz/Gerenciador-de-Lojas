62d0b9cec2184eb0c431cb57366780b434b0edfb
flaviaadiniz <flaviaadiniz@gmail.com>
2023-06-07 16:16:39 -0300

    Exclusão do construtor vazio da classe Vestuario

--- src/main/java/Vestuario.java
+++ src/main/java/Vestuario.java
@@ -2,9 +2,6 @@ public class Vestuario extends Loja {
 
     private boolean produtosImportados;
 
-    public Vestuario() {
-    }
-
     public Vestuario(String nome, int quantidadeFuncionarios, double salarioBaseFuncionario, Endereco endereco,
                      Data dataFundacao, boolean produtosImportados, int quantidadeProdutos) {
         super(nome, quantidadeFuncionarios, salarioBaseFuncionario, endereco, dataFundacao, quantidadeProdutos);
